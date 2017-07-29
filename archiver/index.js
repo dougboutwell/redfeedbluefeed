3760b07bcd25d6a4ff5366353e74fc1628827181
Doug Boutwell <doug@gettotallyrad.com>
2017-07-29 06:25:38 -0700

    Move config data (sites and webshot options) to config.json

--- archiver/index.js
+++ archiver/index.js
@@ -4,85 +4,11 @@ const { join } = require('path');
 const mozjpeg = require('mozjpeg-stream');
 const { streamFile } = require('./google-cloud');
 
-var sites = [
-  {
-    name: 'CNN',
-    url: 'http://cnn.com',
-    shortName: 'cnn'
-  }, {
-    name: 'Fox News',
-    url: 'http://foxnews.com',
-    shortName: 'foxnews'
-  }, {
-    name: 'Wall Street Journal',
-    url: 'http://wsj.com',
-    shortName: 'wsj'
-  }, {
-    name: 'Washington Post',
-    url: 'http://washingtonpost.com',
-    shortName: 'washingtonpost'
-  }, {
-    name: 'NY Times',
-    url: 'http://nytimes.com',
-    shortName: 'nytimes'
-  }, {
-    name: 'Red State',
-    url: 'http://redstate.com',
-    shortName: 'redstate'
-  }, {
-    name: 'Breitbart',
-    url: 'http://breitbart.com',
-    shortName: 'breitbart'
-  }, {
-    name: 'Huffington Post',
-    url: 'http://huffingtonpost.com',
-    shortName: 'huffingtonpost'
-  }, {
-    name: 'USA Today',
-    url: 'http://usatoday.com',
-    shortName: 'usatoday'
-  }, {
-    name: 'Politico',
-    url: 'http://politico.com',
-    shortName: 'politico'
-  }, {
-    name: 'The Hill',
-    url: 'http://thehill.com',
-    shortName: 'thehill'
-  }, {
-    name: 'MSNBC',
-    url: 'http://msnbc.com',
-    shortName: 'msnbc'
-  }
-];
-
-const options = {
-  screenSize: {
-    width: 375,
-    height: 667
-  },
-  shotSize: {
-    width: 'all',
-    height: '2000'
-  },
-  quality: 100,
-  defaultWhiteBackground: true,
-  streamType: 'jpg',
-  timeout: 180 * 1000,
-  userAgent: 'Mozilla/5.0 (iPhone; U; CPU iPhone OS 3_2 like Mac OS X; en-us)'
-    + ' AppleWebKit/531.21.20 (KHTML, like Gecko) Mobile/7B298g'
-};
-
-// function takeScreenshot(site) {
-//   return function (cb) {
-//     console.log(site.name);
-//     webshot(site.url, join(outputPath, site.shortName + '.jpg'), options, cb);
-//   };
-// }
+const config = require('./config.json');
 
 // Returns a stream of the screenshot at url
 function streamScreenshot(url) {
-  return webshot(url, options);
+  return webshot(url, config.options);
 }
 
 // Wrap the streaming snapshot + GCS write into a promise
@@ -108,8 +34,8 @@ async function processAll () {
     sites: []
   };
 
-  for (const i in sites) {
-    const site = sites[i];
+  for (const i in config.sites) {
+    const site = config.sites[i];
     try {
       console.log(site.name);
       site.filePath = join(dstFolder, `${ts}-${site.shortName}.jpg`);
