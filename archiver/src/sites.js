2a1bbb7ae47cc9685b8ebca87c8afb998dd6bd98
Doug Boutwell <doug@gettotallyrad.com>
2017-08-21 21:59:58 -0700

    Speculative fix for occasional Daily Caller fullscreen ads

--- archiver/src/sites.js
+++ archiver/src/sites.js
@@ -310,7 +310,7 @@ const sites = [
     bias: 1,
     webshotOptions: {
       userAgent: 'Mozilla/5.0 (iPhone; CPU iPhone OS 8_0_2 like Mac OS X) AppleWebKit/600.1.4 (KHTML, like Gecko) Version/8.0 Mobile/12A366 Safari/600.1.4',
-      customCSS: '.topad, #masthead, #capture_lightbox_active { display: none !important; }',
+      customCSS: 'iframe, .preloaded_lightbox, .topad, #masthead, #capture_lightbox_active { display: none !important; }',
     }
   },
 
